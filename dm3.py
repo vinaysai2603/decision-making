5916d856f354f5df3570ddb155c6363f2cd8ef23
vinaysai2603 <vinaysai0326@gmail.com>
2024-09-19 01:32:36 -0700

    Create dm3.py

---
+++ dm3.py
@@ -0,0 +1,13 @@
+'''
+Write a program to check whether the given character is vowel or consonant.
+Input format:
+The input consist of a character
+Output format:
+The output consists of a below-given string
+“Vowel” / “Consonant” / “Not an alphabet”
+Sample Input:
+e
+
+Sample Output:
+Vowel
+'''
