4785b43c14fd94617a297892586529703973f95f
vinaysai2603 <vinaysai0326@gmail.com>
2024-09-19 01:34:41 -0700

    Create dm4.py

---
+++ dm4.py
@@ -0,0 +1,14 @@
+'''
+ A fruit seller buys a dozen of banana at Rs.X. He sells 1 banana at Rs.Y. Write a program to determine the profit or loss in Rs. for the fruitseller.
+Input format:
+Input consists of 2 floating point numbers
+The first input corresponds to the total cost(X)
+The second input corresponds to the sold cost(Y)
+Output format:
+Print "Profit or Loss" with Rupees.
+Sample Input:
+60
+4
+Sample Output:
+Loss : Rs.12.00
+'''
