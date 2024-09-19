678055f594cf11f1750db982114fa49d60501284
vinaysai2603 <vinaysai0326@gmail.com>
2024-09-19 01:33:38 -0700

    Update dm3.py

--- dm3.py
+++ dm3.py
@@ -1,13 +1,27 @@
-'''
-Write a program to check whether the given character is vowel or consonant.
-Input format:
-The input consist of a character
-Output format:
-The output consists of a below-given string
-“Vowel” / “Consonant” / “Not an alphabet”
-Sample Input:
-e
+The newly appointed Vice-Chancellor of Anna University wanted to create an automated grading system for the students to check their grade. When a student enters a mark, the grading system displays the corresponding grade.
+Write a program to solve the given problem.
+  Marks scored  
+  Grade 
+100
+S
+90 - 99
+A
+80 - 89
+B
+70 - 79
+C
+60 - 69
+D
+50 - 59
+E
+<50
+F
 
-Sample Output:
-Vowel
-'''
+Input format:
+The input consists of one integer which corresponds to the marks scored by the student
+
+Output format:
+If a student marks greater than 100, print "Invalid Input". Otherwise, print the grade.
+Sample Input:
+78
+Sample Output:C
