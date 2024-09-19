5a3b45f67790137ca0516c85052039693b09cd9e
vinaysai2603 <vinaysai0326@gmail.com>
2024-09-19 01:37:16 -0700

    Create dm7.py

---
+++ dm7.py
@@ -0,0 +1,17 @@
+'''
+There are 3 labs in the CSE department(L1, L2, and L3) with a seating capacity of x, y, and z respectively. Find the lab which has minimal seating capacity.  
+Input format:
+Input consists of 3 integers
+The first input denotes the seating capacity of L1(a)
+The second input denotes the seating capacity of L2(b)
+The third input denotes the seating capacity of L3(c)
+Output format:
+Print the minimal seating lab capacity
+Refer the Sample output for formatting
+Sample Input:
+30
+40
+20
+Sample Output:
+L3
+'''
