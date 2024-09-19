e31305627e58c58d30b94751fe4c7f1de87b7b1c
vinaysai2603 <vinaysai0326@gmail.com>
2024-09-19 01:31:06 -0700

    Create Dm-1.py

---
+++ Dm-1.py
@@ -0,0 +1,14 @@
+'''
+Get two integers x and y from the user and write a program to relate 2 integers as equal to, less than or greater than.
+Input format:
+Input consist of 2 integers
+The first input corresponds to  the first number.(a)
+The second input corresponds to the second number.(b)
+Output format:
+If the first number is equal to the second number, print "x and y are equal". Otherwise, print "x greater than y" or "x less than y"
+Sample Input:
+6
+8
+Sample Output:
+6 less than 8
+'''
