716949d23265bfaa1256e5579ac0472969b966f4
vinaysai2603 <vinaysai0326@gmail.com>
2024-09-19 01:37:47 -0700

    Create dm8.py

---
+++ dm8.py
@@ -0,0 +1,19 @@
+'''
+There are 3 labs in the CSE department are L1, L2, and L3 with a seating capacity of x, y, and z respectively. One of the 3 labs has been allocated for ACE training. Out of the available labs, find the lab which has minimal seating capacity.
+Input format:
+Input consists of 3 integers and a string
+The first input denotes the seating capacity of L1(a)
+The second input denotes the seating capacity of L2(b)
+The third input denotes the seating capacity of L3(c)
+The fourth input denotes the lab which is allocated for ACE training
+Output format:
+Print the minimal seating capacity lab amongst the available labs.
+Refer the Sample output for formatting
+Sample Input:
+30
+40
+20
+L3
+Sample Output:
+L1
+'''
