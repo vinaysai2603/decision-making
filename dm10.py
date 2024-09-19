0c225f0afdace9bbbbe63874c8d73c6473a32228
vinaysai2603 <vinaysai0326@gmail.com>
2024-09-19 01:38:55 -0700

    Create dm10.py

---
+++ dm10.py
@@ -0,0 +1,20 @@
+'''
+
+ There are 3 labs in the CSE department. The labs are L1, L2, and L3 with a seating capacity of x, y, and z respectively. A single lab needs to be allocated to a class of 'n' students. The labs need to be utilized to the maximum i.e the number of systems used should not be minimal. Which lab needs to be allocated to this class?
+Input format:
+Input consists of 4 integers
+The first input denotes 'x'
+The second input denotes 'y'
+The third input denotes 'z'
+The fourth input denotes 'n'
+Output format:
+Print the lab which is suitable for  'n' number of students
+Refer the Sample output for formatting
+Sample Input:
+30
+40
+20
+25
+Sample Output:
+L1
+'''
