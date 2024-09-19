4780fa5cffcea08387eee614704e34859520b0a6
vinaysai2603 <vinaysai0326@gmail.com>
2024-09-19 01:36:42 -0700

    Create dm6.py

---
+++ dm6.py
@@ -0,0 +1,15 @@
+'''
+Ask a user for their birth year encoded as two digits (like "62") and for the current year, also encoded as two digits (like "99"). Write a program to find the users current age in years.
+Input format:
+Input consists of 2 integers
+The first integer corresponds to the last 2 digits of the birth year
+The second integer corresponds to the last 2 digits of the current year
+Output format:
+Print the user's current age
+Refer below sample output for formatting.
+Sample Input:
+62
+00
+Sample Output:
+38
+'''
