816438bbfc31b9a6738b96046377803f21f15a0d
vinaysai2603 <vinaysai0326@gmail.com>
2024-09-19 01:36:11 -0700

    Create dm5.py

---
+++ dm5.py
@@ -0,0 +1,49 @@
+'''
+Write a program to determine the fee amount to be collected from a student. 
+Refer the table below for fee details.
+  Student Type  
+  Student Type denoted as  
+  Fee Details  
+Merit Seat Day Scholar
+MSDS
+
+Tuition fee + Bus fee
+
+
+
+Merit Seat Hosteller
+MSH
+Tuition fee + Hostel fee
+
+Management Seat Day Scholar
+
+
+
+MGSDS
+
+150% of Tuition fee + Bus fee
+
+
+
+Management Seat Hosteller
+MGSH
+
+150% of Tuition fee + Hostel fee
+
+
+
+
+Input format:
+The first input corresponds to the student type
+The second input corresponds to the tuition fee
+The third input corresponds to the bus fee or hostel fees
+Output format:
+Print the total fee amount of the corresponding student with 2 decimal places.
+Refer below sample output for formatting
+Sample Input:
+MSH
+40000
+50000
+Sample Output:
+90000.00
+'''
