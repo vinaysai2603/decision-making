634abb002a278f9897592b076fcd5dc603e84d17
vinaysai2603 <vinaysai0326@gmail.com>
2024-10-03 01:23:03 -0700

    Update dm7.py

--- dm7.py
+++ dm7.py
@@ -15,3 +15,14 @@ Sample Input:
 Sample Output:
 L3
 '''
+L1=int(input())
+L2=int(input())
+L3=int(input())
+if((L1<L2) and (L1<L3)):
+    print("L1")
+elif(L2<L3 and L2<L1):
+    print("L2")
+else:
+    print("L3")
+
+
