203bfcdcb01f49d1dde8700bce7fc53b15286975
vinaysai2603 <vinaysai0326@gmail.com>
2024-10-03 01:23:51 -0700

    Update dm2.py

--- dm2.py
+++ dm2.py
@@ -11,3 +11,11 @@ e
 Sample Output:
 Vowel
 '''
+a=input()
+if(a=='a' or a=='e' or a=='i' or a=='o' or a=='u'):
+    print('Vowel')
+elif(a>'a' and 'a'<a):
+    print("Consonant")
+else:
+    print("Not an alphabet")
+
